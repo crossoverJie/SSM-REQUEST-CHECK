febbcdc86284cf08f163995af5f81e35fff64cf6
crossoverJie <crossoverjie@gmail.com>
2017-05-23 01:15:36 +0800

    Initial commit

---
+++ src/main/java/com/crossoverJie/request/anotation/CheckReqNo.java
@@ -0,0 +1,38 @@
+package com.crossoverJie.request.anotation;
+
+
+import java.lang.annotation.*;
+
+/**
+ *                    _ooOoo_
+ *                   o8888888o
+ *                   88" . "88
+ *                   (| -_- |)
+ *                    O\ = /O
+ *                ____/`---'\____
+ *              .   ' \\| |// `.
+ *               / \\||| : |||// \
+ *             / _||||| -:- |||||- \
+ *               | | \\\ - /// | |
+ *             | \_| ''\---/'' | |
+ *              \ .-\__ `-` ___/-. /
+ *           ___`. .' /--.--\ `. . __
+ *        ."" '< `.___\_<|>_/___.' >'"".
+ *       | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ *         \ \ `-. \_ __\ /__ _/ .-` / /
+ * ======`-.____`-.___\_____/___.-`____.-'======
+ *                    `=---='
+ *
+ * .............................................
+ *          佛祖保佑             永无BUG
+ * @author crossoverJie
+ * Date: 2017/05/20 上午10:51
+ * @since JDK 1.7
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CheckReqNo {
+
+    String desc() default "";
+}
---
+++ src/main/java/com/crossoverJie/request/interceptor/ReqNoDrcAspect.java
@@ -0,0 +1,82 @@
+package com.crossoverJie.request.interceptor;
+
+
+import com.crossoverJie.req.BaseRequest;
+import com.crossoverJie.request.util.StringUtil;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+
+
+@Aspect
+@Component
+public class ReqNoDrcAspect {
+	
+
+	private static Logger logger = LoggerFactory.getLogger(ReqNoDrcAspect.class);
+	private static final String REQNO ="reqNo" ;
+	
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+	
+	@PostConstruct
+	public void init() throws Exception {
+		logger.info("SSM-REQUEST-CHECK init......");
+	}
+	
+	@Pointcut("@annotation(com.crossoverJie.request.anotation.CheckReqNo)")
+	public void checkRepeat(){
+		
+	}
+
+	@Before("checkRepeat()")
+	public void before(JoinPoint joinPoint) throws Exception {
+		BaseRequest request;
+		request = getBaseRequest(joinPoint);
+		if(request != null){
+			final String reqNo = request.getReqNo();
+			if(StringUtil.isEmpty(reqNo)){
+				throw new RuntimeException("reqNo不能为空");
+			}else{
+				try {
+					String tempReqNo = redisTemplate.opsForValue().get(REQNO +reqNo);
+					logger.debug("tempReqNo="+tempReqNo);
+
+					if((StringUtil.isEmpty(tempReqNo))){
+						redisTemplate.opsForValue().set(REQNO +reqNo, reqNo, 1, TimeUnit.DAYS);
+					}else{
+						throw new RuntimeException("请求号重复,reqNo="+reqNo);
+					}
+
+				} catch (RedisConnectionFailureException e){
+					logger.error("redis操作异常",e);
+					throw new RuntimeException("need redisService") ;
+				}
+			}
+		}
+			
+	}
+	
+	
+	 
+	 public static BaseRequest getBaseRequest(JoinPoint joinPoint) throws Exception {
+		 BaseRequest returnRequest = null;
+		 Object[] arguments = joinPoint.getArgs();
+		 if(arguments != null && arguments.length > 0){
+			 returnRequest = (BaseRequest) arguments[0];
+		 }
+	     return returnRequest;
+	 }
+}
